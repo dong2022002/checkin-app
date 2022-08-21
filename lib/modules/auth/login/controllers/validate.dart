633e29bd1a68ee85da06dad127896f36905ff7cc
TranDieuDong <dong200220@gmail.com>
2022-08-21 23:10:49 +0700

    MD5

--- lib/modules/auth/login/controllers/validate.dart
+++ lib/modules/auth/login/controllers/validate.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:checkin_app/models/user.dart';
 import 'package:checkin_app/modules/auth/auth_provider/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:crypto/crypto.dart' as crypto;
 
 class Validate {
   static UserProvider user = UserProvider();
@@ -27,11 +30,14 @@ class Validate {
       List<User> usersdata,
       TextEditingController passController,
       TextEditingController userController) {
+    String passUser = generateMd5(passController.text);
     if (value!.isEmpty) {
       return 'Mật khẩu không được để trống';
     } else {
-      //print(user.user.password);
-      if (user.user.email == passController.text &&
+      print(user.user.matKhau);
+      print(passController.text);
+      print(passUser);
+      if (user.user.matKhau == passUser &&
           user.user.email == userController.text) {
       } else {
         return "Sai mật khẩu";
@@ -39,4 +45,8 @@ class Validate {
     }
     return null;
   }
+}
+
+String generateMd5(String input) {
+  return crypto.md5.convert(utf8.encode(input)).toString();
 }
